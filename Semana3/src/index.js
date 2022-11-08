8e48f1d31edcdac59184c695361526a476b43e52
Rafael Guimarães <67663224+rafaelhguimaraes@users.noreply.github.com>
2022-11-07 22:19:06 -0300

    Insomnia para Requests
    Aprendendo e testando o Insomnia

--- Semana3/src/index.js
+++ Semana3/src/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 // Importando o Express
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello, world!'});
+
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
 });
 
 // setando a rota 3333 
